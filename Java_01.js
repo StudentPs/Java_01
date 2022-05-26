d862eb05d70bc690f9c44c0863b1704cfbe20600
REKTFox <101262858+StudentPs@users.noreply.github.com>
2022-05-27 00:52:13 +0300

    Add files via upload

---
+++ Java_01.js
@@ -0,0 +1,12 @@
+"Привет Ява"
+var myThing
+myThing=5
+myThing="this is would be shown"
+ varnumber = 9
+var string = "9"
+number + number
+string + string
+var greeting = "hello"
+var name1 = "PAVEL"
+greeting + name1
+name1.length;
